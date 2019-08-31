1e3d54b2c8c843470ca46fa6d0b71db2a3ac88f8
ilyadorogin <illya.d.44@gmail.com>
2019-08-31 17:00:52 +0300

    header and first section. adaptive. header with burger menu on small devices

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+
+
+$(document).ready(function(){
+    
+		$('.burger-btn').on('click', function(e) {
+		  e.preventDefault;
+		  $(this).toggleClass('burger-btn_active');
+
+		  	if (!$('nav').hasClass('burger-nav_active')) { 
+    				$('nav').addClass('burger-nav_active');
+    				$('.header-container').addClass('burger-header-container_active'); 
+    				$('header').addClass('burger-header_active');
+    				$('.menu').addClass('burger-menu_active');
+    				console.log('First click');
+ 				 } else { 
+  				$('nav').removeClass('burger-nav_active');
+  				$('.header-container').removeClass('burger-header-container_active'); 
+    			$('header').removeClass('burger-header_active');
+    			$('.menu').removeClass('burger-menu_active');
+    			console.log('Second click'); 
+				}
+		});
+
+
+
+});
+
+$(window).scroll(function() {
+
+    if ( $(window).scrollTop() >= 100 ) {
+        $('header').addClass('header_scrolled');
+    
+    } else {
+        $('header').removeClass('header_scrolled');
+
+    }
+});
